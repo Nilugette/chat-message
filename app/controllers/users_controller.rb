9166f223cc1efc5c0fc1917b22a142e8c8d8b792
Nilugette <nilugette@yahoo.fr>
2020-11-24 13:41:21 +0100

    Add Sign up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    before_action :require_user, except: [:new, :create]
+
+    def new 
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:sucess] = "Welcome to MyChatApp #{@user.username}, you have successfully signed up !"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+end
